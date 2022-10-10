5033d832dbd89bb2179142b1f11c3d0ee15865df
Harit Meryem <houdaharit20@gmail.com>
2022-10-10 03:02:43 +0100

    to re test

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -10,32 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *ptr)
-{
-	int	len;
-
-	len = 0;
-	while (ptr[len] != '\0')
-		len++;
-	return (len);
-}
+#include"libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	p;
-	char	*ptr;
 	int	len;
 
-	ptr = (char *)s;
 	p = (char)c;	
-	len = ft_strlen(ptr);
+	len = ft_strlen(s);
 
 	if (p == '\0')
-		return (&ptr[len]);
+		return (&s[len]);
 	while (len >= 0)
 	{
-		if (ptr[len] == p)
-			return (&ptr[len]);
+		if (s[len] == p)
+			return (&s[len]);
 		len--;
 	}
 	return (0);

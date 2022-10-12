76441184d0ccd57bdf34c3c8e83b75b6b83a38ab
Harit Meryem <haritmeryem@gmail.com>
2022-10-12 13:00:02 +0100

    DONE

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,7 +12,6 @@
 
 #include"libft.h"
 
-
 char	*ft_strnstr(const char *hay, const char *need, size_t len)
 {
 	size_t	first;
@@ -23,47 +22,20 @@ char	*ft_strnstr(const char *hay, const char *need, size_t len)
 	haystack = (char *)hay;
 	needle = (char *)need;
 	first = 0;
-	sec = 0;
-	if (needle[sec] == '\0')
+	if (!needle[0])
 		return (haystack);
-	if (len > 0) //if haystack NULL && len = 0
+	while (haystack[first])
 	{
-	while (haystack[first] != '\0' && len > first)
-	{
-		if (haystack == NULL) // if haystack NULL && len != 0
-			return (NULL);
+		sec = 0;
 		if (haystack[first] == needle[sec])
 		{
-			while (haystack[first] == needle[sec] && needle[sec] != '\0' && len >= first)
-			{
-		//		printf("1/");
+			while (needle[sec] && haystack[first + sec] == needle[sec]
+				&& first + sec < len)
 				sec++;
-				first++;
-			}
-			if (needle[sec] == '\0')
-				return (&haystack[first - sec]);
-			sec = 0;
+			if (!needle[sec])
+				return (&haystack[first]);
 		}
 		first++;
-	//	printf("2/");
-	}
 	}
 	return (NULL);
 }
-
-//haystack NULL && len == 0
-
-/*
-
-#include<string.h>
-#include <stdio.h>
-#include<limits.h>
-
-int main()
-{
-
-    char a[] = "little";
-    char b[] = "little";
-    printf("%s\n",ft_strnstr(NULL,b,0));
-    printf("%s\n",strnstr(NULL,b,0));
-}*/

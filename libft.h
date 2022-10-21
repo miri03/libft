a6f5d0e65ef659b17bd7e6db854b6b9a55dd6706
Meryem Harit <haritmeryem@gmail.com>
2022-10-21 19:53:37 +0100

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: meharit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 22:31:10 by meharit           #+#    #+#             */
-/*   Updated: 2022/10/19 02:22:41 by meharit          ###   ########.fr       */
+/*   Updated: 2022/10/21 01:13:56 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif

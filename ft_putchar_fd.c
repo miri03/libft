5cf33129523419fc6ae7f4773d7f2d64430ac3f5
Meryem Harit <haritmeryem@gmail.com>
2022-10-27 09:34:42 +0100

    DONE

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,11 +6,11 @@
 /*   By: meharit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 21:23:06 by meharit           #+#    #+#             */
-/*   Updated: 2022/10/27 04:40:38 by meharit          ###   ########.fr       */
+/*   Updated: 2022/10/27 08:54:34 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
